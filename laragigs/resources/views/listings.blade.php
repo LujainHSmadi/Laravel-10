146963598946b6e501d31da3997276d020aee81b
LujainHSmadi <lujainhsmadi@gmail.com>
2023-06-23 00:41:36 +0300

    23/06/2023 12:37PM

--- laragigs/resources/views/listings.blade.php
+++ laragigs/resources/views/listings.blade.php
@@ -1,22 +1,19 @@
 @extends('layout')
 @section('content')
-@include('partials._hero')
-@include('partials._search')
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4" >
+    @include('partials._hero')
+    @include('partials._search')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-<h1>{{isset($heading)?$heading:''}}</h1>
-@isset($listings)
+        <h1>{{ isset($heading) ? $heading : '' }}</h1>
+        @isset($listings)
+            @unless (count($listings) == 0)
+                @foreach ($listings as $listing)
+                    <x-listing-card :listing='$listing' />
+                @endforeach
+            @else
+                <p>No Listings Found</p>
+            @endunless
+        @endisset
 
-@unless(count($listings)==0)
-    @foreach ($listings as $listing)
-    <!-- to access the component we use <x-blade-name> -->
-        <x-listing-card :listing = '$listing'></x-listing-card>
-    @endforeach
-    @else
-    <p>No Listings Found</p>
-@endunless
-@endisset
-
-</div>
+    </div>
 @endsection
-
